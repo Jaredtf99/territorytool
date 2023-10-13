5474c77d93a699ced23f42247d8cc03b350df616
Jared <jaredtrapiello@gmail.com>
2023-10-13 22:01:48 +0200

    fix qr camera on mobile, now use back camera

--- angularapp/src/app/components/add-territory/add-territory.component.ts
+++ angularapp/src/app/components/add-territory/add-territory.component.ts
@@ -85,7 +85,8 @@ export class AddTerritoryComponent implements AfterViewInit {
 
     const playDeviceFacingBack = (devices: any[]) => {
       const device = devices.find(f => (/back|rear|environment/gi.test(f.label)));
-      this.action.playDevice(device ? device.deviceId : devices[0].deviceId);
+      const indexToChoose = devices.length > 1 ? 1 : 0;
+      this.action.playDevice(device ? device.deviceId : devices[indexToChoose].deviceId);
     }
 
     this.action[fn](playDeviceFacingBack).subscribe((r: any) => alert);
