06192b533433b1afccdbbb74f8e9746636c382f6
Jared <jaredtrapiello@gmail.com>
2023-12-09 18:55:09 +0100

    change environment for deployment

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://territorytoolapi.azurewebsites.net/api/v1',
-  apiUrl: 'https://localhost:62319/api/v1',
+  apiUrl: 'https://territorytoolapi.azurewebsites.net/api/v1',
+  //apiUrl: 'https://localhost:62319/api/v1',
 };
